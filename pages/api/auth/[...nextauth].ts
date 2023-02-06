dd0ed1ccd6ba6ec273cd627f4294fed8272d6bcf
ben-haas <haas.ben4@gmail.com>
2023-02-06 13:51:29 -0800

    add next-auth with azure ad

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+import AzureADProvider from 'next-auth/providers/azure-ad';
+
+export const authOptions = {
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID as string,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET as string,
+      tenantId: process.env.AZURE_AD_TENANT_ID as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);

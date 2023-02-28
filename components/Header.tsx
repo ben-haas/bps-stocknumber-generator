3a70a8e2e4a4fe6495e56957060a8014ca3e1ce3
ben-haas <haas.ben4@gmail.com>
2023-02-28 15:50:04 -0800

    Migrate header styles to styled components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,7 @@
 import { signOut } from 'next-auth/react';
+import styled from 'styled-components';
 import Link from 'next/link';
 import Image from 'next/image';
-
-import classes from './Header.module.css';
 
 const Header = () => {
   const signOutHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -11,29 +10,62 @@ const Header = () => {
   };
 
   return (
-    <header className={classes.header}>
-      <div className={classes.user}>
-        <Link href={'/'}>
+    <Wrapper>
+      <div>
+        <StyledLink href={'/'}>
           <Image src="/logo_large.png" alt="logo" width="210" height="75" />
-        </Link>
+        </StyledLink>
       </div>
       <nav>
-        <ul>
-          <li>
-            <Link href={'/'}>New Number</Link>
-          </li>
-          <li>
-            <Link href={'/numbers'}>View Items</Link>
-          </li>
-          <li>
+        <UL>
+          <LI>
+            <StyledLink href={'/'}>New Number</StyledLink>
+          </LI>
+          <LI>
+            <StyledLink href={'/numbers'}>View Items</StyledLink>
+          </LI>
+          <LI>
             <button className="btn" onClick={signOutHandler}>
               Logout
             </button>
-          </li>
-        </ul>
+          </LI>
+        </UL>
       </nav>
-    </header>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.header`
+  width: auto;
+  height: 5rem;
+  background-color: #0d47a1;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10%;
+`;
+
+const UL = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const LI = styled.li`
+  margin: 0 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+
+  &:hover {
+    color: #d6943f;
+  }
+`;
 
 export default Header;

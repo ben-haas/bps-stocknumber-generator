df0b56aea6f9c3844b0471e8b9a68ecee71aff34
ben-haas <haas.ben4@gmail.com>
2023-04-06 10:20:08 -0700

    remove azure auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,8 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
-import AzureADProvider from 'next-auth/providers/azure-ad';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID as string,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET as string,
-      tenantId: process.env.AZURE_AD_TENANT_ID as string,
-    }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,

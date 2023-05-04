2a67442fe664e39bb8d22f5c0079eac3bf4a82f5
ben-haas <haas.ben4@gmail.com>
2023-05-04 14:19:37 -0700

    Change detail page to form, begin wiring together

--- components/Numbers/NumberDetail.tsx
+++ components/Numbers/NumberDetail.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard } from '@fortawesome/free-regular-svg-icons';
 
 import Card from '../UI/Card';
-import styled from 'styled-components';
+import FormStatus from './FormStatus';
 import Button from '../UI/Button';
 
 import { COLORS } from '@/styles/constants';
@@ -20,7 +24,19 @@ interface DetailProps {
 }
 
 const NumberDetail: React.FC<DetailProps> = ({ numberData, onEditNumber }) => {
+  const { data: session } = useSession();
   const [isEditing, setIsEditing] = useState(false);
+  const [readOnly, setReadOnly] = useState(true);
+  const [prodLine, setProdLine] = useState(numberData.productLine);
+  const [stockNum, setStockNum] = useState(numberData.stockNumber.toString());
+  const [prodCode, setProdCode] = useState(numberData.productCode);
+  const [lockedPCode, setLockedPCode] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [isValid, setIsValid] = useState({
+    pLine: true,
+    pCode: true,
+    form: false,
+  });
 
   const formatDate = (date: string) => {
     const newDate = new Date(date).toLocaleString('en-US', {
@@ -33,20 +49,22 @@ const NumberDetail: React.FC<DetailProps> = ({ numberData, onEditNumber }) => {
 
   const onEditHandler = () => {
     setIsEditing(true);
+    setReadOnly(false);
   };
 
   const onCancelHandler = () => {
     setIsEditing(false);
+    setReadOnly(true);
   };
 
   const onUpdateHandler = () => {
     if (!isEditing) return;
     const newData = {
-      stock_number: numberData.stockNumber,
-      product_code: 'ZZZ-024437',
-      product_line: 'ZZZ',
+      stock_number: stockNum,
+      product_code: prodCode,
+      product_line: prodLine,
       last_edited: new Date(),
-      edited_by: 'test',
+      edited_by: session?.user?.name,
       created_at: numberData.createdAt,
       entered_by: numberData.user,
       is_typical: numberData.isTypical,
@@ -54,48 +72,168 @@ const NumberDetail: React.FC<DetailProps> = ({ numberData, onEditNumber }) => {
     onEditNumber(numberData.stockNumber, newData);
   };
 
+  const onProdLineChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProdLine(e.target.value.toUpperCase());
+    if (e.target.value.toUpperCase().match('[A-Z]{3}') === null) {
+      setIsValid({ ...isValid, pLine: false, form: false });
+      setProdCode('');
+      return;
+    }
+    setSubmitted(false);
+
+    setProdCode(`${e.target.value.toUpperCase()}-0${stockNum}`);
+    setIsValid({ pLine: true, pCode: true, form: true });
+  };
+
+  const pCodeChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProdCode(e.target.value.toUpperCase());
+    if (e.target.value.toUpperCase().match('[A-Z]{3}[-][0-9]{6}') === null) {
+      setIsValid({ ...isValid, pCode: false });
+      return;
+    }
+
+    if (e.target.value.toUpperCase().substring(0, 3) != prodLine) {
+      setIsValid({ ...isValid, pCode: false });
+      return;
+    }
+    setIsValid({ ...isValid, pCode: true });
+  };
+
+  const copyButtonHandler = () => {
+    navigator.clipboard.writeText(prodCode);
+  };
+
+  const checkBoxHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (lockedPCode && !e.target.checked && prodLine != '') {
+      setProdCode(`${prodLine}-0${stockNum}`);
+      setIsValid({ ...isValid, pCode: true });
+    }
+
+    if (lockedPCode && !e.target.checked && prodLine === '') {
+      setProdCode('');
+      setIsValid({ ...isValid, pCode: true });
+    }
+
+    setLockedPCode(e.target.checked);
+  };
+
   return (
     <DetailCard>
-      <Wrapper>
-        <h3>Product Line</h3>
-        <p>{numberData.productLine}</p>
-        <h3>Stock Number</h3>
-        <p>{numberData.stockNumber}</p>
-        <h3>Product Code</h3>
-        <p>{numberData.productCode}</p>
-        <h3>Created By</h3>
-        <p>{numberData.user}</p>
-        <h3>Created At</h3>
-        <p>{formatDate(numberData.createdAt)}</p>
-        <h3>Last Edited</h3>
-        <p>{formatDate(numberData.lastEdited)}</p>
-        <ButtonWrapper>
-          <Button onClick={onEditHandler}>Edit</Button>
-          <CancelButton show={isEditing} onClick={onCancelHandler}>
-            Cancel
-          </CancelButton>
-          <UpdateButton show={isEditing} onClick={onUpdateHandler}>
-            Update
-          </UpdateButton>
-        </ButtonWrapper>
-      </Wrapper>
+      <NumberForm>
+        <Control>
+          <label htmlFor="prodLine">Product Line</label>
+          <NumberInput
+            value={prodLine}
+            id="prodLine"
+            type="text"
+            size={5}
+            maxLength={3}
+            readOnly={readOnly}
+            onChange={onProdLineChangeHandler}
+            valid={isValid.pLine}
+          />
+        </Control>
+        <Control>
+          <label htmlFor="stockNum">Stock Number</label>
+          <NumberInput
+            value={stockNum}
+            id="stockNum"
+            type="text"
+            size={10}
+            readOnly={readOnly}
+            valid={true}
+          />
+        </Control>
+        <Control>
+          <label htmlFor="newProdCode">Product Code</label>
+          <InputWrapper>
+            <NumberInput
+              value={prodCode}
+              onChange={pCodeChangeHandler}
+              id="newProdCode"
+              style={{ cursor: readOnly ? 'not-allowed' : 'text' }}
+              type="text"
+              size={12}
+              maxLength={10}
+              readOnly={readOnly}
+              valid={isValid.pCode}
+            />
+            <Clipboard icon={faClipboard} onClick={copyButtonHandler} />
+          </InputWrapper>
+        </Control>
+        <Control>
+          <CustomCheck
+            id="customCheck"
+            type="checkbox"
+            checked={lockedPCode}
+            onChange={checkBoxHandler}
+          />
+          <label htmlFor="customCheck">Custom Product Code</label>
+        </Control>
+      </NumberForm>
+      <Actions>
+        <Button onClick={onEditHandler}>Edit</Button>
+        <CancelButton show={isEditing} onClick={onCancelHandler}>
+          Cancel
+        </CancelButton>
+        <UpdateButton show={isEditing} onClick={onUpdateHandler}>
+          Update
+        </UpdateButton>
+      </Actions>
     </DetailCard>
   );
 };
 
 const DetailCard = styled(Card)`
   justify-content: center;
+  flex-direction: column;
 `;
-const Wrapper = styled.div`
+const Actions = styled.div`
   text-align: center;
 `;
 
-const ButtonWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-  justify-content: center;
-  margin-top: 1rem;
+const NumberForm = styled.form`
+  position: flex;
+`;
+
+const Control = styled.div`
+  margin-bottom: 1rem;
+
+  label {
+    font-weight: bold;
+  }
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NumberInput = styled.input<{ valid: boolean }>`
+  padding: 0.35rem;
+  border-radius: 4px;
+  background-color: ${(p) => (p.valid ? COLORS.blueGray50 : COLORS.error100)};
+  border: 1px solid ${(p) => (p.valid ? COLORS.blueGray50 : COLORS.error900)};
+  display: block;
+  font-size: 1.25rem;
+  text-transform: uppercase;
+`;
+
+const Clipboard = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  font-size: 1.25rem;
+  cursor: pointer;
+`;
+
+const CustomCheck = styled.input`
+  height: 16px;
+  width: 16px;
+  margin-right: 16px;
+  vertical-align: middle;
 `;
 
 const UpdateButton = styled(Button)<{ show: boolean }>`
